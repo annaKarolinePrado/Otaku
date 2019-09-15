3002870894861fe6dcc14f3506fc95fba02b684d
victordossouza <55245688+victordossouza@users.noreply.github.com>
2019-09-15 11:42:47 -0300

    Add files via upload

---
+++ Controller/cartão/save.php
@@ -0,0 +1,31 @@
+<?php
+	include('conexao.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			
+			$titular = $_POST['titular'];
+			$numero = $_POST['numero'];
+			$chaveSeguranca = $_POST['chaveSeguranca'];
+			$usuarioId = $_POST['usuarioId'];
+			
+			
+			$sql = "INSERT INTO cartao VALUES (null, '$titular', '$numero','$chaveSeguranca','$usuarioId')";
+			
+			$query = mysqli_query($con, $sql);
+			if($query) {
+				echo "Cartão cadastrado com sucesso!";
+			} else {
+				echo "Não foi possível cadastrar o cartão. Erro: " . mysqli_error($query);
+			}
+		?>
+	</body>
+</html>
+<?php
+	mysqli_close($con);
+?>

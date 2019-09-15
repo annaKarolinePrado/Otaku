90e47b6317f2945beb5e6e0240ebf53cf7ec5013
annaKarolinePrado <annakaroline9298@gmail.com>
2019-09-15 12:08:43 -0300

    Criado script do banco

---
+++ Controller/usuario/salvar.php
@@ -0,0 +1,15 @@
+<?php
+	include('../../conexao/conexao.php');
+	
+	$nome = $_POST['nome'];	
+	
+    $sql = "INSERT INTO usuario VALUES (null, '$nome', '$login', '$senha', $perfil)";
+	$query = mysqli_query($con, $sql);
+	if($query) {
+            header('Location: ../../views/produtora/create.php?status=1');
+	} else {
+            header('Location: ../../views/produtora/create.php?status=2');
+	}
+	
+	mysqli_close($con);
+?>

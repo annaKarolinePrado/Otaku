57973c4db0e335db73e54528fc215bbf6db0e6f3
victordossouza <55245688+victordossouza@users.noreply.github.com>
2019-09-14 23:29:28 -0300

    Add files via upload

---
+++ Controler/cadastrar_like_db.php
@@ -0,0 +1,29 @@
+<?php
+	include('conexao.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>--</title>
+	</head>
+	<body>
+		<?php
+			
+			$nivel = $_POST['nivel'];
+			$ativo = $_POST['ativo'];
+			
+			
+			$sql = "INSERT INTO like VALUES (null, '$nivel', '$ativo')";
+			
+			$query = mysqli_query($con, $sql);
+			if($query) {
+				echo "nivel cadastrado com sucesso!";
+			} else {
+				echo "Não foi possível cadastrar o nivel. Erro: " . mysqli_error($query);
+			}
+		?>
+	</body>
+</html>
+<?php
+	mysqli_close($con);
+?>
